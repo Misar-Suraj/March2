b7712d904397fca468aecfcf57d8037706be4149
Misar-Suraj <surajmisar1995@gmail.com>
2022-03-06 18:06:26 +0530

    0.03.2022

---
+++ InvescoPerpetual/src/LogicalPrograms.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+// Palindrome
+public class LogicalPrograms 
+{
+   public static void main(String[] args)
+   {
+      String a="sam";
+      String b="mas";
+      
+      
+   }
+}
